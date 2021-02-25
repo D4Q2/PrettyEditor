7cb47a62fd612c447f383e33eabac341b830ee23
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2021-02-25 10:15:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -123,15 +123,5 @@ function setupTemplate() {
   var textFileContent = request.responseText; 
   
   var code = document.querySelector('.editor');
-  code.innerText = "
-<html>
-  <head>
-    <title>New</title>
-  </head>
-  <body>
-        Your stuff here.
-  </body>
-</html>
-      
-"
+  code.innerText = "<html><head><title>New</title></head><body>Your stuff here.</body></html>"
 }
