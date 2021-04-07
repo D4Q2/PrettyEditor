0d08644b7b7e5a7ac75e48ffbd6fc156f07ebc46
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2021-04-07 15:52:00 -0400

    Update OnOffEditor.js

--- OnOffEditor.js
+++ OnOffEditor.js
@@ -1,15 +1,15 @@
 let editorHtml = '<div class="editor" contenteditable="true" spellcheck="false"></div><button onclick="compile()" class="runButton">Run</button><button onclick="setupTemplate()" class="tempButton">Template</button><div class="background"><iframe id="code"></iframe></div><script src="main.js"></script>'
                                  
-function activateEditor(var name) {
-  if (document.getElementById(name).innerHTML == "") {
+function activateEditor(name) {
+  if (document.getElementById(name).innerHTML == '') {
       // The editor is not there, create it
       document.getElementById(name).innerHTML == editorHtml;
   } 
-  else if (document.getElementById(name).innerHTML.contains("<button onclick="compile()" class="runButton">Run</button>")) {
+  else if (document.getElementById(name).innerHTML.contains('<button onclick="compile()" class="runButton">Run</button>')) {
       // The editor is there, remove it
-      document.getElementById(name).innerHTML == "";
+      document.getElementById(name).innerHTML == '';
   }
   else {
-    console.log("There is a problem with code editor area. It has an unexpected inner html.");
+    console.log('There is a problem with code editor area. It has an unexpected inner html.');
   }
 }
