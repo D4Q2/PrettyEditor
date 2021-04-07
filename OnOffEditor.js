24d92af593f9dbf4fc5b78bc87666f168f935451
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2021-04-07 16:29:10 -0400

    Update OnOffEditor.js

--- OnOffEditor.js
+++ OnOffEditor.js
@@ -3,6 +3,19 @@ let editorHtml = '<div class="editor" contenteditable="true" spellcheck="false">
 
 function activateEditor(name) {
   var codeAreaHtml = document.getElementById(name).innerHTML;
+  if (codeAreaHtml.includes('<button onclick="compile()" class="runButton">Run</button>')) {
+    console.log("The editor is active, destroying it...");
+    // The editor is there, remove it
+    const myNode = document.getElementById(name);
+    myNode.innerHTML = '';
+    //document.getElementById(name).innerHtml == '';
+  }
+  else {
+    console.log('No editor, creating one...');
+    // The editor is not there, create it
+    document.getElementById(name).innerHTML == editorHtml;
+  }
+ /*
   console.log(name);
   if (codeAreaHtml == '') {
     console.log('No editor, creating one...');
@@ -19,4 +32,5 @@ function activateEditor(name) {
   else {
     console.log('There is a problem with code editor area. It has an unexpected inner html.');
   }
+  */
 }
