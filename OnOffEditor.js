c09504dcfdc0a453249be9fa5e4c1c562a1a4138
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2021-04-07 15:16:09 -0400

    Create OnOffEditor.js

---
+++ OnOffEditor.js
@@ -0,0 +1,24 @@
+let editorHtml = "
+<div class="editor" contenteditable="true" spellcheck="false">
+      </div>
+      <button onclick="compile()" class="runButton">Run</button>
+      <button onclick="setupTemplate()" class="tempButton">Template</button>
+      <div class="background">
+        <iframe id="code"></iframe>
+      </div>
+      <script src="main.js"></script>
+                                 "
+                                 
+function activateEditor(var name) {
+  if (document.getElementById(name).innerHTML == "") {
+      // The editor is not there, create it
+      document.getElementById(name).innerHTML == editorHtml;
+  } 
+  else if (document.getElementById(name).innerHTML == editorHtml) {
+      // The editor is there, remove it
+      document.getElementById(name).innerHTML == "";
+  }
+  else {
+    console.log("There is a problem with code editor area. It has an unexpected inner html.");
+  }
+}
